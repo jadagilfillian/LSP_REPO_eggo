87b7d4a1bf7a2a68ad828ab106f933af5ff08bda
jadag <jadag@LAPTOP-S1FTC8E0>
2025-08-19 21:15:45 -0400

    This is assignment one for LSP.

--- LSP_Fall_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Fall_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,6 +3,9 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		/**
+		 * Jada Gilfillian
+		 */
 		System.out.println("Hello World!!");
 
 	}
